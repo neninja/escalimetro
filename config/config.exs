a467272ab8ed51d5ff972cf4b2a428b35d223ab5
Neni <felipe.dacs@gmail.com>
2026-05-31 01:55:09 -0300

    :new: mix phx.gen.auth Accounts User users

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :escalimetro, :scopes,
+  user: [
+    default: true,
+    module: Escalimetro.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Escalimetro.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :escalimetro,
   ecto_repos: [Escalimetro.Repo],
   generators: [timestamp_type: :utc_datetime]
--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -35,4 +35,45 @@ defmodule EscalimetroWeb.ConnCase do
     Escalimetro.DataCase.setup_sandbox(tags)
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
+
+  @doc """
+  Setup helper that registers and logs in users.
+
+      setup :register_and_log_in_user
+
+  It stores an updated connection and a registered user in the
+  test context.
+  """
+  def register_and_log_in_user(%{conn: conn} = context) do
+    user = Escalimetro.AccountsFixtures.user_fixture()
+    scope = Escalimetro.Accounts.Scope.for_user(user)
+
+    opts =
+      context
+      |> Map.take([:token_authenticated_at])
+      |> Enum.into([])
+
+    %{conn: log_in_user(conn, user, opts), user: user, scope: scope}
+  end
+
+  @doc """
+  Logs the given `user` into the `conn`.
+
+  It returns an updated `conn`.
+  """
+  def log_in_user(conn, user, opts \\ []) do
+    token = Escalimetro.Accounts.generate_user_session_token(user)
+
+    maybe_set_token_authenticated_at(token, opts[:token_authenticated_at])
+
+    conn
+    |> Phoenix.ConnTest.init_test_session(%{})
+    |> Plug.Conn.put_session(:user_token, token)
+  end
+
+  defp maybe_set_token_authenticated_at(_token, nil), do: nil
+
+  defp maybe_set_token_authenticated_at(token, authenticated_at) do
+    Escalimetro.AccountsFixtures.override_token_authenticated_at(token, authenticated_at)
+  end
 end
